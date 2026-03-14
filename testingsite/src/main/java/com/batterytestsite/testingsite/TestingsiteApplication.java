bc603d5ab78557822a101fb06ad727fcefe03b6f
megawego <91439645+megawego@users.noreply.github.com>
2026-03-14 15:24:55 -0400

    Added test code for opening the site.

--- testingsite/src/main/java/com/batterytestsite/testingsite/TestingsiteApplication.java
+++ testingsite/src/main/java/com/batterytestsite/testingsite/TestingsiteApplication.java
@@ -2,6 +2,9 @@ package com.batterytestsite.testingsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class TestingsiteApplication {
@@ -9,5 +12,8 @@ public class TestingsiteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestingsiteApplication.class, args);
 	}
-
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 }
