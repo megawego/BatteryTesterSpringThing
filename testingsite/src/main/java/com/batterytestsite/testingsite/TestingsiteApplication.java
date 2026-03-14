b2dd2f0c073f3352fc8740fdb7987c7a4828dc90
Matthew <12280@stjoebears.com>
2026-03-14 13:05:03 -0400

    Set Up Java Workspace Using Spring.

---
+++ testingsite/src/main/java/com/batterytestsite/testingsite/TestingsiteApplication.java
@@ -0,0 +1,13 @@
+package com.batterytestsite.testingsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingsiteApplication.class, args);
+	}
+
+}
---
+++ testingsite/src/test/java/com/batterytestsite/testingsite/TestingsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batterytestsite.testingsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
